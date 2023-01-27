ddf98b5d01c242b28b6b2c0f84862033ffacc016
Erick Rocha <erick2003rocha@gmail.com>
2023-01-26 23:35:20 -0300

    add courses module

---
+++ src/courses/courses.controller.ts
@@ -0,0 +1,41 @@
+import { CoursesService } from './courses.service';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
+import {
+  Body,
+  Delete,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common/decorators';
+
+@Controller('courses')
+export class CoursesController {
+  constructor(private readonly coursesService: CoursesService) {}
+
+  @Get()
+  findAll() {
+    return this.coursesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.coursesService.findOne(id);
+  }
+
+  @Post('create')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  create(@Body() body) {
+    return this.coursesService.create(body);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return this.coursesService.update(id, body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.coursesService.remove(id);
+  }
+}
